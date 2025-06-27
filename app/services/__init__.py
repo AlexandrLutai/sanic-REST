c8c13a859f66ab4730f6868cd3a1662a561e68bc
alex89620109009 <alex89620109009@gmail.com>
2025-06-27 18:27:07 +0300

    Добавлен WebhookService в инициализацию модуля сервисов

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,9 +1,13 @@
 """Инициализация модуля сервисов"""
 
-from .user_service import UserService, AccountService, PaymentService
+from .user_service import UserService
+from .account_service import AccountService
+from .payment_service import PaymentService
+from .webhook_service import WebhookService
 
 __all__ = [
     "UserService",
     "AccountService", 
-    "PaymentService"
+    "PaymentService",
+    "WebhookService"
 ]
